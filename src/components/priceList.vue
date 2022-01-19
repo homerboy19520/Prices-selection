ae54bf2074e6a3fda23a22b04ebf7476ba42d044
Артур Филимонцев <homerboy19520@mail.ru>
2022-01-19 15:11:13 +0300

    deploy no github

---
+++ src/components/priceList.vue
@@ -0,0 +1,192 @@
+<template>
+  <div class="price-list">
+    <h4 class="price-list__level">{{ level }}</h4>
+    <h3 class="price-list__price">${{ price }}</h3>
+    <ul class="price-list__list">
+      <li class="price-list__item" v-for="(item, index) in info" :key="index">
+        <p class="price-list__info">{{ item.text }}</p>
+      </li>
+    </ul>
+    <a class="price-list__button">
+      <p class="price-list__content">LEARN MORE</p></a
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "price-list",
+  props: {
+    level: {
+      type: String,
+      require: true,
+    },
+    price: {
+      type: Number,
+      require: true,
+    },
+    info: {
+      type: Array,
+      require: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.price-list {
+  position: relative;
+  box-sizing: border-box;
+  width: 327px;
+  padding: 31px 29px;
+  box-shadow: 0px 20px 40px rgba(212, 210, 244, 0.5);
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 32px;
+  opacity: 1;
+  cursor: pointer;
+  transition: padding 0.2s ease-out;
+
+  & h4,
+  & h3,
+  &__info,
+  .price-list__content,
+  .price-list__info {
+    transition: color 0.2s ease-out;
+  }
+
+  &::after {
+    position: absolute;
+    z-index: -1;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(135deg, #a2a7f0 0.06%, #696edd 100.06%);
+    opacity: 0;
+    transition: opacity 0.2s ease-out;
+    border-radius: 10px;
+  }
+
+  &::before {
+    position: absolute;
+    z-index: -1;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    transition: opacity 0.2s ease-out;
+    border-radius: 10px;
+  }
+
+  &:hover {
+    &::after {
+      opacity: 1;
+    }
+
+    & h4,
+    & h3,
+    .price-list__info {
+      color: #fff;
+    }
+
+    .price-list__content {
+      color: #6d72de;
+    }
+
+    .price-list__button::after {
+      opacity: 0;
+    }
+
+    @media (min-width: 1110px) {
+      padding-top: 50px;
+      padding-bottom: 50px;
+    }
+  }
+
+  &__level {
+    font-size: 18px;
+    line-height: 28px;
+    color: #6e728e;
+    text-align: center;
+  }
+
+  &__price {
+    font-size: 72px;
+    line-height: 71px;
+    letter-spacing: -2.16px;
+    color: #4a4d60;
+    text-align: center;
+  }
+
+  &__info {
+    padding: 13px 0;
+    font-size: 15px;
+    line-height: 28px;
+    text-align: center;
+    color: #6e728e;
+  }
+
+  &__list {
+    padding: 0;
+    margin: 0;
+  }
+
+  &__item {
+    list-style: none;
+    border-top: 1px solid #dbdce3;
+    cursor: pointer;
+    box-sizing: border-box;
+    width: 288px;
+
+    &:last-child {
+      border-bottom: 1px solid #dbdce3;
+    }
+  }
+
+  &__button {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 6px;
+    width: 100%;
+    min-height: 44px;
+
+    &::after {
+      position: absolute;
+      z-index: 0;
+      content: "";
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(135deg, #a2a7f0 0.06%, #696edd 100.06%);
+      transition: opacity 0.2s ease-out;
+      border-radius: 6px;
+      opacity: 1;
+    }
+
+    &::before {
+      position: absolute;
+      z-index: 0;
+      content: "";
+      width: 100%;
+      height: 100%;
+      background: #fff;
+      transition: opacity 0.2s ease-out;
+      border-radius: 6px;
+      opacity: 1;
+    }
+  }
+
+  &__content {
+    font-size: 13px;
+    line-height: 16px;
+    text-align: center;
+    letter-spacing: 1.39286px;
+    color: #fff;
+    z-index: 1;
+  }
+}
+</style>
---
+++ src/pages/Pricing.vue
@@ -0,0 +1,262 @@
+<template>
+  <div class="pricing-wrapper">
+    <div class="pricing-header">
+      <h2 class="pricing-header__title">Our Pricing</h2>
+      <div class="pricing-header__container">
+        <p class="pricing-header__size">Annually</p>
+        <div
+          class="radio"
+          @click="toogle"
+          :class="{ 'm-monthly': IsMonthly }"
+        ></div>
+        <p class="pricing-header__size">Monthly</p>
+      </div>
+    </div>
+
+    <div class="pricing-container">
+      <priceList
+        v-for="(item, index) in mokData"
+        :key="index"
+        :level="item.level"
+        :price="item.price"
+        :info="item.info"
+      />
+    </div>
+
+
+
+    <div
+    v-for="index in 2"
+    :key="index"
+    :class="['pricing-header__box', {'pricing-header__box_screen': index === 1}]">
+      <img
+        class="pricing-header__img"
+        src="@/assets/background-image.png"
+        alt="background-image"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import priceList from "@/components/priceList.vue";
+
+export default {
+  name: "Pricing",
+  components: {
+    priceList,
+  },
+  data() {
+    return {
+      procent: 20,
+      IsMonthly: true,
+      staticPrice: [],
+      mokData: [
+        {
+          level: "Basic",
+          price: 19.99,
+          info: [
+            { text: "500 GB Storage" },
+            { text: "2 Users Allowed" },
+            { text: "Send up to 3 GB" },
+          ],
+        },
+        {
+          level: "Professional",
+          price: 24.99,
+          info: [
+            { text: "1 TB Storage" },
+            { text: "5 Users Allowed" },
+            { text: "Send up to 10 GB" },
+          ],
+        },
+        {
+          level: "Master",
+          price: 39.99,
+
+          info: [
+            { text: "2 TB Storage" },
+            { text: "10 Users Allowed" },
+            { text: "Send up to 20 GB" },
+          ],
+        },
+      ],
+    };
+  },
+
+  methods: {
+    toogle: function () {
+      this.IsMonthly = !this.IsMonthly;
+
+      if (this.IsMonthly == false) {
+        this.makeDiscount();
+      } else {
+        this.makeReverse();
+      }
+    },
+
+    makeDiscount: function () {
+      this.mokData.forEach((item) => {
+        let fullPriceMonthly = Math.ceil(item.price);
+        let fullPriceAnnualy = fullPriceMonthly * 10;
+        let priceWithDiscount =
+          fullPriceAnnualy * ((100 - this.procent) / 100);
+        item.price = priceWithDiscount;
+      });
+    },
+
+    makeReverse: function () {
+      this.mokData.forEach((item, index) => {
+        item.price = this.staticPrice[index];
+      });
+    },
+  },
+  mounted() {
+    this.mokData.forEach((item) => {
+      this.staticPrice.push(item.price);
+    });
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 700;
+  background-color: #f7f7fe;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+p {
+  margin: 0;
+}
+
+.pricing-wrapper {
+  position: relative;
+  padding: 64px 24px 71px 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  gap: 80px;
+  overflow: hidden;
+}
+
+.pricing-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  gap: 40px;
+
+  &__title {
+    color: #6e728e;
+    font-size: 32px;
+    line-height: 39px;
+  }
+
+  &__box {
+    position: absolute;
+    z-index: -2;
+    right: -280px;
+    top: -130px;
+    width: 470px;
+    height: 760px;
+
+    &_screen {
+      display: none;
+
+      @media (min-width: 600px) {
+        position: absolute;
+        z-index: -2;
+        display: block;
+        left: -11%;
+        top: 74%;
+        transform: scale(1, -1);
+      }
+    }
+  }
+
+  &__img {
+    width: 100%;
+    height: 100%;
+  }
+
+  &__container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row;
+    gap: 24px;
+  }
+
+  &__size {
+    font-size: 15px;
+    line-height: 28px;
+    color: #6e728e;
+    opacity: 0.5;
+  }
+}
+
+.radio {
+  display: block;
+  position: relative;
+  box-sizing: border-box;
+  width: 52px;
+  height: 32px;
+  background: linear-gradient(135deg, #a2a7f0 0%, #696edd 100%);
+  border-radius: 16px;
+  cursor: pointer;
+
+  &.m-monthly::after {
+    left: calc(100% - 28px);
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    background-color: #fff;
+    border-radius: 50%;
+    left: 4px;
+    top: 50%;
+    transform: translate(0, -50%);
+    transition: left 0.2s ease-out;
+  }
+
+  &__label {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    cursor: pointer;
+  }
+
+  &__checkbox {
+  }
+}
+.pricing-container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  gap: 32px;
+
+  @media (min-width: 900px) {
+    justify-content: space-around;
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+
+  @media (min-width: 1110px) {
+    justify-content: center;
+    align-items: center;
+    height: 550px;
+  }
+}
+</style>
